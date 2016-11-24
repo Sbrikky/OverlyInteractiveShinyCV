aab9bf2d341e4c28fcf9e54c6e97fe6f02b85a27
Sbrikky <marijn.stevering@sbs.nl>
2016-11-24 11:10:24 +0100

    Added some ideas
    Added some ideas as comments at the bottom of the server.R script

--- server.R
+++ server.R
@@ -21,3 +21,23 @@ shinyServer(function(input, output) {
   })
 
 })
+
+#### Ideas ####
+# Datatable with all positions, and some info: Startdate, enddate, length, used skills/technologies, teamsize, hours, stuffssss!
+# A text box that gives detailed information about the position currently selected in the positionstable
+# Similar table for edumucationizers
+# 
+# Some multilevel chart showing breakdown of skills, for example techs at first level and then relevant topics in subfield
+#   so for example at large: 80% R, 10% Scala, 5% Python, 5% misc, maybe GIS in there somewhere, I dunno, STUFF!
+#   And then R might be divided into: 20% visualisation, 20% Machine learning, 20% data munging and 20% interactive Shiny stuff
+#
+# Try and include Stack Overflow data 
+#   best question and answer, 
+#   another multilevel chart showing breakdown of score for different tags
+# 
+# Something displaying character traits with 
+#   way of choosing one and getting additional information/examples competency style
+#     Ex.: analytic/pragmatic mindset -> the hypthetical case of studying different bus routes (Or spelltoweR if that works)
+#   
+# 
+# 
